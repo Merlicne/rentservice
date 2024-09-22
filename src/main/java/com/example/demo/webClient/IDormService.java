d3221ff3ef0d8f4a20b916c61a721cc091e06f97
Supatz317 <supatsorn.su@kkumail.com>
2024-09-22 19:42:42 +0700

    retry dependency

--- src/main/java/com/example/demo/webClient/IDormService.java
+++ src/main/java/com/example/demo/webClient/IDormService.java
@@ -1,11 +1,16 @@
 package com.example.demo.webClient;
 
+import org.springframework.retry.annotation.Retryable;
+
 import com.example.demo.model.BuildingModel;
 import com.example.demo.model.DormModel;
 import com.example.demo.model.JwtToken;
 
 public interface IDormService {
+    @Retryable(value = { Exception.class }, maxAttempts = 5)
     public BuildingModel getBuilding(int id, JwtToken token);
+
+    @Retryable(value = { Exception.class }, maxAttempts = 5)
     public DormModel getDormInfo(JwtToken token);
 
 }
--- src/main/java/com/example/demo/webClient/IRoomService.java
+++ src/main/java/com/example/demo/webClient/IRoomService.java
@@ -1,8 +1,11 @@
 package com.example.demo.webClient;
 
+import org.springframework.retry.annotation.Retryable;
+
 import com.example.demo.model.JwtToken;
 import com.example.demo.model.RoomModel;
 
 public interface IRoomService {
+    @Retryable(value = { Exception.class }, maxAttempts = 5)
     public RoomModel getRoom(int id, JwtToken token);
 }
